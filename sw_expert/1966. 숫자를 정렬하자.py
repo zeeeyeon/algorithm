ccb2424157e9115034de787dab19c23422900347
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-05 00:08:51 +0900

    정렬 구현 및 속도 비교(버블, 선택, 카운팅)

--- sw_expert/1966. 숫자를 정렬하자.py
+++ sw_expert/1966. 숫자를 정렬하자.py
@@ -1,11 +1,11 @@
 T = int(input())
 
-for case in range(1, T + 1):
+for case in range(1, 2):
 
     N = int(input())
     arr = list(map(int, input().split()))
 
-    # 선택 정렬
+    # 선택 정렬(121ms)
     for i in range(N-1):
        min_index = i
        for j in range(i+1, N):
@@ -14,11 +14,30 @@ for case in range(1, T + 1):
 
        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-    # 버블 정렬
+    # 버블 정렬(126ms)
     for i in range(N - 1, 0, -1):
         for j in range(i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    # 카운팅 정렬 (108ms)
+    # 1. 최대값보다 1크게 만들기
+    max_v = max(arr)
+    data = [0] * (max_v + 1)
+
+    # 2. 순회하면서 순서대로 넣어주기
+    for i in range(N):
+        data[arr[i]] += 1
+
+    # 3. 길이만큼의 배열 만들어 주기
+    new_arr = [0] * len(arr)
+
+    # 4. 순회하면서 넣어주기
+    index = 0
+    for i in range(len(data)):
+        for _ in range(data[i]):
+            new_arr[index] = i
+            index += 1
 
     print(f'#{case}', end=' ')
     print(*arr)
