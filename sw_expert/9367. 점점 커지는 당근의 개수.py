77e09c2fa1655ccf7b954b8d90d8836486c79efb
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-04 17:47:14 +0900

    당근 뽑기

--- sw_expert/9367. 점점 커지는 당근의 개수.py
+++ sw_expert/9367. 점점 커지는 당근의 개수.py
@@ -1,26 +1,31 @@
 T = int(input())
-
-for case in range(1, T+1):
+for case in range(1, T + 1):
     number = int(input())
     carrots = list(map(int, input().split()))
 
-    result = []
-    count = 0
-    for i in range(1, number):
-        if carrots[i-1] < carrots[i]:
+    count = 1
+    max_count = 1
+    for i in range(number-1):
+        if carrots[i] < carrots[i+1]:
             count += 1
-        else :
-            count = 0
-        result.append(count)
+            if max_count < count : max_count = count
+        else : count = 1
 
-    # 횟수가 같은 경우 큰 값 체크
-    max_v = max(result)
-    index = [index for index, value in enumerate(result) if max_v == value]
+    print(f'{case} {max_count}')
 
-    big_carrot = 0
-    for i in index:
-        if big_carrot < carrots[result.index(big_carrot) + 1]:
-            big_carrot = carrots[result.index(big_carrot) + 1]
-
-    if max(result) == 0: big_carrot = 1
-    print(big_carrot)
+    # count = 0
+    # result = [0]
+    # for i in range(1, number):
+    #     if carrots[i - 1] < carrots[i]: count += 1
+    #     else : count = 0
+    #     result.append(count)
+    #
+    # max_equal = []
+    # max_v = max(result)
+    # for i in range(len(result)):
+    #     if max_v <= result[i]: max_equal.append(carrots[i])
+    #
+    # if max(result) == 0: result_number = 1
+    # else : result_number = max(max_equal)
+    #
+    # print(f'#{case} {result_number}')
