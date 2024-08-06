1cd3793256a65090fc6dd001ac81f5b9bacc319a
jiyeon_0342 <jiyeon_0342@naver.com>
2024-08-06 16:27:56 +0900

    슬라이스 활용해서 풀기

---
+++ sw_expert/21618.반복문자 지우기.py
@@ -0,0 +1,23 @@
+# T = int(input())
+#
+# def peek(stack):
+#     if len(s) != 0:return s[-1]
+#
+# # def get_stack_len(s):
+# #     return len(s)
+# #
+# # if get_stack_len(s):
+# #     peek(s)
+#
+# def check(s):
+#
+#     stack = []
+#
+#     for char in s:
+#
+#
+#
+# for case in range(1, T+1):
+#     s = map(str, input())
+#
+#     print(f'#{case} {check(s)}')
--- sw_expert/2805. 농작물 수확하기.py
+++ sw_expert/2805. 농작물 수확하기.py
@@ -1,25 +1,23 @@
 T = int(input())
+
 for case in range(1, T+1):
     N = int(input())
     data = [list(map(int, input())) for _ in range(N)]
-
     '''
         5x5인 경우,
         mid = 2, i == 2까지는 i행에 대한 j의 값은 [mid - i : mid + i + 1]으로 계산하고
         i행의 인덱스가 mid(2)를 넘는 순간 j의 계산이 [mid - i : mid + i + 1] 가능해지려면
-        i에 i % mid 나머지 값을 넣어서 계산해줘야 한다고 생각합니당...! 
+        i에 i % mid 나머지 값을 넣어서 계산해줘야 한다고 생각 ^.^&
     '''
-
-    total = 0
     mid = N // 2
-    for i in range(N):
-       for j in range(N):
-           if mid < i:
-               k = i % mid
-               if mid - k <= j <= mid + k:
-                   total += data[i][j]
-           else :
-               if mid - i <= j <= mid + i:
-                   total += data[i][j]
+    total = 0
 
-    print(total)
+    for i in range(N):
+        if i <= mid:
+            total += sum(data[i][mid - i: mid + i + 1])
+        else:
+            k = i - mid
+            total += sum(data[i][k: N-k])
+
+    print(f'#{case} {total}')
+
