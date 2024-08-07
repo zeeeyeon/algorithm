4b7966ee08fa33c2c7b228fffd432f21c0fd7532
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-07 18:01:07 +0900

    stack

--- sw_expert/21650. 기능개발.py
+++ sw_expert/21650. 기능개발.py
@@ -1,30 +1,45 @@
+import math
+
 def solution(progresses, speeds):
     time = []
     for i in range(len(progresses)):
-        if progresses[i] % speeds[i] == 0:
-            time.append((100 - progresses[i]) // speeds[i])
-        else:
-            time.append(((100 - progresses[i]) // speeds[i]) + 1)
+        time.append(math.ceil(100 - progresses[i]) // speeds[i])
 
+    print(time)
     answer = []
-    i = 0
     count = 1
     for i in range(1, len(time)):
-        # time[-1] 확인
-        if time[i] >= time[i+1]: count += 1
-        else:
+        if time[i-1] < time[i]:
             answer.append(count)
             count = 1
+        else :
+            count += 1
     answer.append(count)
-
     return answer
+
+    print(solution([93, 30, 55], [1, 30, 5]))
+    print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
 
 # def solution(progresses, speeds):
 #     answer = []
 #     COMPLETE = 100
+#     count = 0
 #
-#     for i in range(len(progresses)):
-#         if progresses[i] > COMPLETE: continue
-#         else: answer[i] =
+#     while speeds:
+#         for i in range(len(speeds)):
+#             # 한 사이클 돌았을 때 첫번째 요소가 100이 넘지 않으면 100이상 될때까지 계속 사이클 돌리기
+#             if progresses[i] >= COMPLETE: continue
+#             progresses[i] += speeds[i]
+#
+#         while progresses and progresses[0] >= COMPLETE:
+#             progresses.pop(0)
+#             count += 1
+#
+#             if progresses and progresses[0] < COMPLETE:
+#                 answer.append(count)
+#                 count = 0
+#
+#             elif len(progresses) == 0: answer.append(count)
+#             speeds.pop(0)
 #
 #     return answer
