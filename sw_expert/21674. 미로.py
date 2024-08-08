f37bb8e295d9b9830d78411faf8dcf08d8aa835b
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-08 22:44:04 +0900

    dfs활용하여 미로탈출

--- sw_expert/21674. 미로.py
+++ sw_expert/21674. 미로.py
@@ -1,13 +1,13 @@
 def dfs(x, y, visited):
-    # global result
+    global result
 
     # 도착지점에 닿으면 끝
     if maze[x][y] == END:
-        # result = 1
-        return 1
+        result = 1
+
 
     # 도착한 길 표시하기
-    visited[x][y] = False
+    visited[x][y] = True
 
     # 0으로 되어있는 길로만 갈 수 있음
     for dx, dy in dxy:
@@ -19,11 +19,11 @@ def dfs(x, y, visited):
         # 이동하려는 좌표가 벽일 경우
         if maze[nx][ny] == WALL: continue
         # 방문했던 곳인지 확인
-        if not visited[nx][ny]: continue
+        if visited[nx][ny]: continue
 
-        if dfs(nx, ny, visited): return 1
+        dfs(nx, ny, visited)
 
-    return 0
+    return result
 
 
 T = int(input())
@@ -33,7 +33,7 @@ for case in range(1, T+1):
     N = int(input())
     maze = [list(map(int, input())) for _ in range(N)]
 
-    visited = [[True] * N for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
 
     WALL, START, END = 1, 2, 3
     x, y, result = 0, 0, 0
@@ -42,9 +42,9 @@ for case in range(1, T+1):
         for j in range(N):
             if maze[i][j] == 2:
                 x, y = i, j
+                break
 
     # 출발지에서 갈 수 있는 길 검색
-    visited[x][y] = False
-    a = dfs(x, y, visited)
+    visited[x][y] = True
 
-    print(f'#{case} {a}')
+    print(f'#{case} {dfs(x, y, visited)}')
