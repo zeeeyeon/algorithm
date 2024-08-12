51a70a0a44eb8e078d5fa7191c9276def6f1e6e6
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-13 01:23:15 +0900

    전치행렬

--- sw_expert/1220. Magnetic.py
+++ sw_expert/1220. Magnetic.py
@@ -1,9 +1,16 @@
-T = int(input())
+T = 10
 
 for case in range(1, T+1):
     length = int(input())
     arr = [list(map(int, input().split())) for _ in range(length)]
+    count = 0
 
-    # 1 = N, 2 = S
-    # 열에서 원소가 하나면 넘기기
-    #
+    new_arr = list(zip(*arr))
+    for col in new_arr:
+        prev = 0
+        for i in col:
+            if i:
+                if i == 2 and prev == 1:
+                    count += 1
+                prev = i
+    print(f'#{case} {count}')
