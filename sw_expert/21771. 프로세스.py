aac9105bd954304009c9ded86b5d021941b8db5f
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-14 00:22:05 +0900

    queue 활용

--- sw_expert/21771. 프로세스.py
+++ sw_expert/21771. 프로세스.py
@@ -6,38 +6,33 @@ def solution(priorities, location):
     for index, value in enumerate(priorities):
         arr.append([index, value])
 
-    # print(arr)
     # 실행 횟수를 담을 변수
     # count = 0
     # while arr:
-    #     print(arr)
     #     # 배열의 첫번째 요소를 제거, 변수에 담음
     #     current = arr.pop(0)
-    #     print(current)
+    #     # 배열의 요소중 가장 중요도가 큰 값인지 확인 flag
+    #     flag = True
     #     # current의 중요도를 arr 요소들의 중요도와 하나씩 비교, current의 중요도보다 큰 값(더 중요도가 큰 값)이 존재한다면 뒤로 보내기(append)
     #     for i in range(len(arr)):
     #         #current = [0, 1] => 1이 중요도를 나타냄 => current[1] , arr[i][1]
     #         if current[1] < arr[i][1]:
-    #             #
     #             arr.append(current)
-    #             print(arr)
-    #             count -= 1
-    #
-    #     count += 1
-    #     print(count)
-    #     if current[1] == location:
-    #         return count
+    #             # 중요도가 가장 큰 값을 찾음
+    #             flag = False
+    #             break
+    #     if flag:
+    #         count += 1
+    #         if current[1] == location:
+    #             return count
     count = 0
     while arr:
-        for i in range(1, length):
-            if arr[0][1] < arr[i][0]:
-                temp = arr.pop(0)
-                arr.append(temp)
+        current = arr.pop(0)
+        if max(current[1] < important[1] for important in arr):
+            arr.append(current)
+        else :
+            count += 1
+            if current[0] == location: return count
 
-
-
-
-
-
-# print(solution([2, 1, 3, 2], 2))
+print(solution([2, 1, 3, 2], 2))
 print(solution([1, 1, 9, 1, 1, 1], 0))
