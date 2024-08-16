5a3ade5d23d9280d93d1bc3b959fa8f87421dc17
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-16 12:24:07 +0900

    im

---
+++ sw_expert/2605. 줄 세우기.py
@@ -0,0 +1,14 @@
+students = int(input())
+waiting = list(map(int, input().split()))
+
+order = []
+for i in range(students):
+    seat = len(order)
+    if seat == 0: order.append(i+1)
+    else :order.insert(seat - waiting[i], i+1)
+
+for i in order:
+    print(i, end=' ')
+
+
+
