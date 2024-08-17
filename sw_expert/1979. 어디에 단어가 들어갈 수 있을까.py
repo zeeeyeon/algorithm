1302ead1117cecd1164de4c27aa18113c4ecc971
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-17 16:48:37 +0900

    zip, zero padding

--- sw_expert/1979. 어디에 단어가 들어갈 수 있을까.py
+++ sw_expert/1979. 어디에 단어가 들어갈 수 있을까.py
@@ -1,25 +1,24 @@
-dxy = [[0, 1], [1, 0]]
-
-def check_puzzle(x, y, arr, dx, dy):
-    count = 0
-    for i in range(1, 4):
-        nx = x + dx * i
-        ny = y + dy * i
-        if nx < 0 or nx >= N or ny < 0 or ny >= N: break
-        if arr[nx][ny] == 1: count += 1
-
-    return count == 3
+# 1을 만나면 count += 1, 0을 만나면 count, k 같은지 확인
+# 같다면 total += 1, 다르면 count = 0 초기화
+def check_one(arr, N, K):
+    total = 0
+    for i in range(N+1):
+        count = 0
+        for j in range(N+1):
+            if arr[i][j] == 1: count += 1
+            else:
+                if count == K:
+                    total += 1
+                count = 0
+    return total
 
 for case in range(1, int(input()) + 1):
     N, K = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr = [list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (N+1)]
+    #  zip을 사용하여 반대로 돌리기
+    arr_reverse = list(map(list, zip(*arr)))
 
-    total = 0
-    for dx, dy in dxy:
-        for i in range(N):
-            for j in range(N):
-                if arr[i][j] == 1:
-                    if check_puzzle(i, j, arr, dx, dy): total += 1
+    result = check_one(arr, N, K)
+    result_reverse = check_one(arr_reverse, N, K)
 
-
-    print(total)
+    print(f'#{case} {result + result_reverse}')
