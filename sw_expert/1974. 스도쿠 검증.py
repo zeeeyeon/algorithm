f726a28e325ab0a2aa02b9731ea810d28593b032
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-18 20:38:17 +0900

    list

--- sw_expert/1974. 스도쿠 검증.py
+++ sw_expert/1974. 스도쿠 검증.py
@@ -1,1 +1,27 @@
+import sys
+sys.stdin = open('input.txt')
 
+def check():
+    for number in arr:
+        if len(set(number)) != N:
+            return 0
+
+    arr_reverse = list(zip(*arr))
+    for number in arr_reverse:
+        if len(set(number)) != N:
+            return 0
+
+    for i in range(0, N, 3):
+        for j in range(0, N, 3):
+            number_list = arr[i][j:j+3] + arr[i+1][j:j+3] + arr[i+2][j:j+3]
+            if len(set(number_list)) != N:
+                return 0
+    return 1
+
+for case in range(1, int(input())):
+    N = 9
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    result = check()
+
+    print(f'#{case} {result}')
