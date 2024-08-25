f515fa16d09015ea038464f11020d09df9447444
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-25 22:18:54 +0900

    dfs + backtracking

--- sw_expert/21718. 배열 최소 합.py
+++ sw_expert/21718. 배열 최소 합.py
@@ -1,27 +1,25 @@
-def find_min_sum(depth, current_sum):
+def find_min_sum(x, current_sum):
     global min_sum
 
-    # if depth == N:
-    #     min_sum = min(min_sum, current_sum)
-    #     return
-    #
-    # for i in range(N):
-    #     if not visited[i]:
-    #         visited[i] = True
-    #         find_min_sum(depth + 1, current_sum + arr[depth][i])
-    #         visited[i] = False
+    if min_sum <= current_sum: return
+
+    if x == N:
+        min_sum = min(min_sum, current_sum)
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            find_min_sum(x + 1, current_sum + arr[x][i])
+            visited[i] = False
 
 T = int(input())
-
 for case in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     visited = [False] * N
-    min_sum = 0
+    min_sum = float('inf')
 
-    print(visited)
-
-    # find_min_sum(depth=0, current_sum=0)
-
-    # print(min_sum)
+    find_min_sum(x = 0, current_sum = 0)
+    print(f'#{case} {min_sum}')
