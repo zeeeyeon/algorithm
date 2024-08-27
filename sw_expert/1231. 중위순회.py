d205a781097dd096c8644527e29bb6dcac4c8842
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-27 23:46:20 +0900

    binary tree

--- sw_expert/1231. 중위순회.py
+++ sw_expert/1231. 중위순회.py
@@ -1,11 +1,28 @@
+def str_to_int(number):
+    try:
+        return int(number)
+    except ValueError:
+        return number
+
+def search(node):
+    global word
+
+    if node:
+        search(arr[node][2])
+        word += arr[node][1]
+        search(arr[node][3])
+
 for case in range(1, 11):
     N = int(input())
-    arr = [list(map(str, input().split())) for _ in range(N)]
+    arr = [[0, 0, 0, 0]] + [list(map(str, input().split())) for _ in range(N)]
+    word = ''
 
-    # word = []
-    # for i in range(len(arr)):
-    #     word.append(arr[i][1])
+    for node_arr in arr:
+        for index, value in enumerate(node_arr):
+            node_arr[index] = str_to_int(value)
 
-    #isdecimal 안쓰는거 추천~
-    a = list(map(list, zip(*arr)))
-    print(a)
+            while len(node_arr) != 4:
+                node_arr.append(0)
+    search(1)
+
+    print(f'#{case} {word}')
