341cb7bc56b1836df95e9fcdd160ad6857a821f4
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-29 23:22:36 +0900

    binary heap

--- sw_expert/1232. 사칙연산.py
+++ sw_expert/1232. 사칙연산.py
@@ -1,21 +1,32 @@
-def search(node):
-    if tree[node]:
-        if node*2<=N:search(node*2)
-        if (node*2+1)<=N:search(node*2+1)
-        result.append(tree[node])
+def search(num):
+    if not tree[num][0] and not tree[num][1]:
+        return int(node[num])
 
+    left = search(tree[num][0])
+    right = search(tree[num][1])
 
+    if node[num] == '+':
+        return left + right
+    if node[num] == '-':
+        return left - right
+    if node[num] == '*':
+        return left * right
+    if node[num] == '/':
+        return left / right
 
-for case in range(1, 2):
+for case in range(1, 11):
     N = int(input())
-    arr = [list(map(str, input().split())) for _ in range(N)]
-    tree = [0] * (N+1)
-    result = []
 
-    for i in range(N):
-        if arr[i][1].isdecimal():
-            tree[i+1] = int(arr[i][1])
-        else :
-            tree[i+1] = arr[i][1]
+    # 각 노드에 값 넣기
+    node = [''] * (N+1)
+    # 자손 가지고 있는 노드들 넣기
+    tree = [[0, 0] for _ in range(N+1)]
 
-    search(1)
+    for i in range(1, N+1):
+        arr = list(input().split())
+        node[i] = arr[1]
+        if len(arr) == 4:
+            tree[i][0] = int(arr[2])
+            tree[i][1] = int(arr[3])
+
+    print(f'#{case} {int(search(1))}')
