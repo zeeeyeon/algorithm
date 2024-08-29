7334b90b7cc43af78815ba79c49c047613013cee
zeeeyeon <zeeyeonleee@gmail.com>
2024-08-29 10:01:04 +0900

    binary heap node_sum

--- sw_expert/5178. 노드의 합.py
+++ sw_expert/5178. 노드의 합.py
@@ -3,9 +3,17 @@ for case in range(1, int(input()) + 1):
     N, M, L = map(int, input().split())
     # 리프 노드 번호, 벨류 값
     arr = [list(map(int, input().split())) for _ in range(M)]
-
+    result = 0
     tree = [0] * (N+1)
+
     for item in arr:
-        tree[item[0]//2].append([item[1]])
-        # print(tree[item[0]//2])
-    print(tree)
+        tree[item[0]] = item[1]
+
+
+    while N != L*2-1:
+        child = N
+        parent = N // 2
+        tree[parent] += tree[child]
+        N -= 1
+
+    print(f'#{case} {tree[L]}')
