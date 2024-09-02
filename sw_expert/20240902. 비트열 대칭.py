0d0f4da9e759c9b8d9a766d2e91ab8492f3a82ad
zeeeyeon <zeeyeonleee@gmail.com>
2024-09-02 16:00:44 +0900

    '완전탐색'

--- sw_expert/20240902. 비트열 대칭.py
+++ sw_expert/20240902. 비트열 대칭.py
@@ -1,19 +1,23 @@
-for T in range(1, int(input()) + 1):
-    N = int(input())
-    arr = list(map(int, input()))
+# for T in range(1, int(input()) + 1):
+#     N = int(input())
+#     arr = list(map(int, input()))
+#
+#     result = 0
+#     for i in range(1, N-1):
+#         left, right, total = i-1, i+1, 1
+#
+#         while left >= 0 and right < N:
+#             if arr[left] == arr[right]:
+#                 total += 2
+#                 left -= 1
+#                 right += 1
+#             else:
+#                 result = max(result, total)
+#                 break
+#         result = max(result, total)
+#
+#     print(f'#{T} {result}')
 
-    result = 0
-    for i in range(1, N-1):
-        left, right, total = i-1, i+1, 1
 
-        while left >= 0 and right < N:
-            if arr[left] == arr[right]:
-                total += 2
-                left -= 1
-                right += 1
-            else:
-                result = max(result, total)
-                break
-        result = max(result, total)
+path = []
 
-    print(f'#{T} {result}')
