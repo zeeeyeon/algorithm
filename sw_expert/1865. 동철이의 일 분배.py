398e81e4cca92d6ea6f92b898819420abd905005
zeeeyeon <zeeyeonleee@gmail.com>
2024-09-05 10:04:01 +0900

    'dfs'

--- sw_expert/1865. 동철이의 일 분배.py
+++ sw_expert/1865. 동철이의 일 분배.py
@@ -1,30 +1,16 @@
-# def search_max_percent(level, current_sum):
-#     global max_percent
-#
-#     if max_percent <= current_sum
-#
-#
-# for x in range(1, int(input()) + 1):
-#     level = int(input())
-#
-#     visited = [False] * level
-#     max_percent = float('inf')
-#
-#     search_max_percent(level, current_sum = 0)
-
 def find_min_sum(x, current_sum):
-    global min_sum
+    global max_sum
 
-    if min_sum <= current_sum: return
+    if max_sum >= current_sum: return
 
     if x == N:
-        min_sum = min(min_sum, current_sum)
+        max_sum = max(max_sum, current_sum)
         return
 
     for i in range(N):
         if not visited[i]:
             visited[i] = True
-            find_min_sum(x + 1, current_sum + arr[x][i])
+            find_min_sum(x + 1, current_sum * arr[x][i]/100)
             visited[i] = False
 
 T = int(input())
@@ -33,7 +19,7 @@ for case in range(1, T+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
 
     visited = [False] * N
-    min_sum = float('inf')
+    max_sum = 0
 
-    find_min_sum(x = 0, current_sum = 0)
-    print(f'#{case} {min_sum}')
+    find_min_sum(0, 1)
+    print(f'#{case} {max_sum * 100:.6f}')
