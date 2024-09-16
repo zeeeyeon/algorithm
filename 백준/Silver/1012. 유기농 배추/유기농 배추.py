d196d341f7d68625473a695b3a182b8b002a65bf
zeeeyeon <zeeyeonleee@gmail.com>
2024-09-16 22:44:22 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 114144 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def bfs(ground, x, y):
+    queue = deque()
+    queue.append([x, y])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            nx, ny = x + dx, y + dy
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= M: continue
+            if ground[nx][ny]: queue.append([nx, ny])
+            ground[nx][ny] = 0
+
+    return 1
+
+for case in range(1, int(input()) + 1):
+    N, M, B = map(int, input().split())
+    ground = [[0] * M for _ in range(N)]
+    count = 0
+
+    for _ in range(B):
+        x, y = map(int, input().split())
+        ground[x][y] = 1
+
+    for x in range(N):
+        for y in range(M):
+            if ground[x][y]:
+                count += bfs(ground, x, y)
+
+    print(count)
