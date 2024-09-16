f8a8a9c096de68e198bdcc787d184b79fd6e1ed4
zeeeyeon <zeeyeonleee@gmail.com>
2024-09-16 22:46:23 +0900

    bfs memory overflow

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -12,6 +12,7 @@ def bfs(ground, x, y):
 
             if nx < 0 or nx >= N or ny < 0 or ny >= M: continue
             if ground[nx][ny]: queue.append([nx, ny])
+            # 같은 좌표를 여러번 넣는 것을 방지, 미리 0으로 변경 후 큐에서 뽑기 (메모리 초과남 !!!!!)
             ground[nx][ny] = 0
 
     return 1
