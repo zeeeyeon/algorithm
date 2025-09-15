b0b14151faba89d7771851d3867a4f4d5a0f4266
zeeeyeon <zeeyeonleee@gmail.com>
2025-09-15 22:10:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,12 +1,32 @@
 def solution(n):
-    word = "수"
-    words = "수박"
+    word100 = "수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박"
+    word10 = "수박수박수박수박수박"
+    word4 = "수박수박"
+    word2 = "수박"
+    odd_word = "수"
+    result = ""
     
-    if n == 1:
-        return word
-    
-    if n % 2 == 0:
-        return (n // 2) * words
-    else:
-        return (n // 2) * words + word
+    if n > 100 :
+        temp = n // 100
+        n = n % 100
+        result = result + (word100 * temp)
+    if n > 10 :
+        temp = n // 10
+        n = n % 10
+        result = result + (word10 * temp)
+        
+    if n > 4 :
+        temp = n // 4
+        n = n % 4
+        result = result + (word4 * temp)
+        
+    if n >= 2 :
+        temp = n // 2
+        n = n % 2
+        result = result + (word2 * temp)
+        
+    if n == 1 :
+        result = result + odd_word
+        
+    return result
     
